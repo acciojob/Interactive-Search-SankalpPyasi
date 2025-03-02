4f4136319a3f69af11861d43f40d3a3e5df20efa
SankalpPyasi <sankalppyasi6@gmail.com>
2025-03-02 22:10:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const search = document.querySelector('.search');
+        const input = document.querySelector('.input');
+        const btn = document.querySelector('.btn');
+
+        btn.addEventListener('click', () => {
+            search.classList.toggle('active'); // Toggle active class
+            input.focus(); // Focus on the input field
+        });
